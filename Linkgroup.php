a4f4fb6e21932be56745cd86a7d0fc5f39001801
David Öhlin <hello@davidohl.in>
2018-05-16 17:02:15 +0200

    change name to linkgroup :link:

---
+++ Linkgroup.php
@@ -0,0 +1,38 @@
+<?php 
+
+namespace Modularity\Module\Linkgroup;
+
+class Linkgroup extends \Modularity\Module
+{
+    public $slug = 'mod-Linkgroup';
+    public $nameSingular = 'Linkgroup';
+    public $namePlural = 'Linkgroups';
+    public $description = 'Linkgroup module for Modularity';
+    public $templateDir = LINKGROUP_MODULE_PATH . 'templates';
+
+    public function init()
+    {
+
+        $this->nameSingular = __('Linkgroup', 'modularity');
+        $this->namePlural =   __('Linkgroups', 'modularity');
+        $this->description =  __('Outputs Linkgroup', 'modularity');
+    }
+
+    public function template()
+    {
+        return 'modularity-mod-linkgroup.blade.php';
+    }
+
+    public function data() : array
+    {
+        return get_fields($this->ID);
+    }
+
+    public function script()
+    {
+        // Not working :(
+        // https://github.com/helsingborg-stad/Modularity/issues/34
+        wp_register_script('mod-linkgroup-js', plugins_url( '/assets/js/mod-linkgroup.js', __FILE__ ), array(), '', true);
+        wp_enqueue_script('mod-linkgroup-js');
+    }
+}
