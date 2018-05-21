8e4fdc85df3f236ad6c91e3a129c877fc6dd9d18
David Öhlin <hello@davidohl.in>
2018-05-21 17:08:47 +0200

    use h2 instead of h3 :cd:

--- templates/modularity-mod-linkgroup.blade.php
+++ templates/modularity-mod-linkgroup.blade.php
@@ -1,5 +1,5 @@
 @if(isset($items))
-	<h3 id={!! sanitize_title($post_title) !!}>{!! apply_filters('the_title', $post_title) !!}</h3>
+	<h2 id={!! sanitize_title($post_title) !!}>{!! apply_filters('the_title', $post_title) !!}</h2>
 	<ul>
 		@foreach ($items as $item)
 			<li><a href="{{ $item['url'] }}">{{ $item['title'] }}</a></li>
