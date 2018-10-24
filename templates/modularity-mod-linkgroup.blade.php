3ad9efcc61ecdc3afc033676d5fb057b9e0bb518
Albin Martinsson <albin@digitaliseringsbyran.se>
2018-10-24 08:47:34 +0200

    Add linkgroup margin

--- templates/modularity-mod-linkgroup.blade.php
+++ templates/modularity-mod-linkgroup.blade.php
@@ -3,7 +3,7 @@
 		<span class="border-b-2 border-blue-dark text-xl font-bold text-black pb-2 z-20 relative leading-none">{!! apply_filters('the_title', $post_title) !!}</span>
 		<hr class="absolute pin-b pin-l w-full h-0 border-b-2 mb-1 border-blue-light z-10">
 	</header>
-	<ul class="list-reset">
+	<ul class="list-reset mb-8">
 		@foreach ($items as $item)
 			<li class="mb-2">
 				<svg class="h-4 w-4 align-middle mr-1">
