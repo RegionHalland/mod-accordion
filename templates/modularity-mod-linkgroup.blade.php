e7fbe89d4e229744949932cbecec0f3707ec64c2
Albin Martinsson <albin@digitaliseringsbyran.se>
2018-10-31 15:21:20 +0100

    Add new styles to avoid multiline bug

--- templates/modularity-mod-linkgroup.blade.php
+++ templates/modularity-mod-linkgroup.blade.php
@@ -1,15 +1,14 @@
 @if(isset($items) && !empty($items))
-	<header id={!! sanitize_title($post_title) !!} class="relative pb-4 block mb-4">
-		<span class="border-b-2 border-blue-dark text-xl font-bold text-black pb-2 z-20 relative leading-none">{!! apply_filters('the_title', $post_title) !!}</span>
-		<hr class="absolute pin-b pin-l w-full h-0 border-b-2 mb-1 border-blue-light z-10">
+	<header id={!! sanitize_title($post_title) !!} class="relative pb-2 block mb-2">
+		<span class="border-b border-grey-light block text-xl font-bold text-black pb-2 z-20 relative">{!! apply_filters('the_title', $post_title) !!} aksodaksod</span>
 	</header>
 	<ul class="list-reset mb-8">
 		@foreach ($items as $item)
-			<li class="mb-2">
-				<svg class="h-4 w-4 align-middle mr-1">
+			<li class="inline-flex mb-4">
+				<svg class="h-4 w-4 align-bottom mr-2">
 					<use xlink:href="#link-2"/>
 				</svg>
-				<a class="text-lg text-blue-dark" href="{{ $item['url'] }}">{{ $item['title'] }}</a>
+				<a class="text-lg text-blue-dark flex-1 " href="{{ $item['url'] }}">{{ $item['title'] }}</a>
 			</li>
 		@endforeach
 	</ul>
